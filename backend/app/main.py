68f2bc7c7c72cadd5357a587e0532164395e4bf8
Matt <listennn08@outlook.com>
2025-05-10 14:01:45 +0800

    refactor: rename to controller

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,8 +24,8 @@ app.add_middleware(
 
 
 def mount_routers(app: FastAPI):
-    from api.route import router
-    from api.sockets import sio_app
+    from controllers.route import router
+    from controllers.sockets import sio_app
 
     app.include_router(router)
     app.mount("/socket.io", app=sio_app)
